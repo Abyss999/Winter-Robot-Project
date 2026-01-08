318ed8bc39f2b31bd6a73e0753b982f205f4b8b4
ISucc34 <kriskrid27@outlook.com>
2026-01-07 23:55:17 -0500

    Hardware done

---
+++ robot/GPIOzero_logic.py
@@ -0,0 +1,52 @@
+from gpiozero import Motor, Robot
+import curses
+from time import sleep
+
+left_motor = Motor(forward=23, backward=22, enable=25)  # Motor1 pins
+right_motor = Motor(forward=17, backward=18, enable=24)  # Motor2 pins
+
+robot = Robot(left=left_motor, right=right_motor)
+
+def test_motors():
+    robot.forward()
+    sleep(2)
+    robot.stop()  
+
+"""actions = {
+    curses.KEY_UP: robot.forward,
+    curses.KEY_DOWN: robot.backward,
+    curses.KEY_LEFT: robot.left,
+    curses.KEY_RIGHT: robot.right,
+    ord('q'): exit
+}"""
+
+actions = {
+    ord('w'): robot.forward,
+    ord('s'): robot.backward,
+    ord('a'): robot.left,
+    ord('d'): robot.right,
+    ord('q'): exit
+}
+
+
+
+def main(window):
+    next_key = None
+    while True:
+        curses.halfdelay(1)
+        if next_key is None:
+            key = window.getch()
+        else:
+            key = next_key
+            next_key = None
+        if key != -1:
+            curses.halfdelay(1)
+            action = actions.get(key)
+            if action is not None:
+                action()
+            next_key = key
+            while next_key == key:
+                next_key = window.getch()
+            robot.stop()
+    
+curses.wrapper(main)
